711948565d0bbba1bab4c42ec51a087a6c649574
グエン　ミン　チ <tringuyenminh209@gmail.com>
2026-03-08 02:00:32 +0900

    feat: Initialize Next.js configuration with standalone output and experimental typed routes.

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    outputFileTracingRoot: path.join(__dirname, '../../'),
+    outputFileTracingRoot: path.join(__dirname, '../../../'),
     reactStrictMode: true,
     experimental: {
         typedRoutes: true,
